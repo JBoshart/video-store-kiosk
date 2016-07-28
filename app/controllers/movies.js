c81ef933f73fb385b3e64a9b52115144cbd620d4
Jillian Boshart <Jilliliz@gmail.com>
2016-07-28 14:24:15 -0700

    Movie posters are now clickable. They don't do anything but log to the console, but hey! That is something! Whoo.

--- app/controllers/movies.js
+++ app/controllers/movies.js
@@ -13,49 +13,50 @@ export default Ember.Controller.extend({
         this.set('page', (this.page - 1));
       }
     },
-    handleClick(event) {
+    selectMovie(event) {
       event.preventDefault()
-
+      console.log("I clicked it.")
 
     }
   }
 });
 
-$(document).ready(function () {
-  console.log('ready!')
-
-  $('.controls button').on('click', handleClick)
-})
-
-function handleClick(event) {
-  event.preventDefault()
-  let currentPage = $('#songs').data('current-page')
-
-  getPage(currentPage + 1)
-}
-
-function getPage(pageNumber) {
-  $.ajax('http://localhost:3000/songs', {
-    type: 'GET',
-    data: {
-      page: pageNumber
-    }
-  }).done(function (data, statusCode, hxrGarbage) {
-    console.log("Data is data", data)
-
-    $('#songs').html('')
-
-    for (let item of data) {
-      $('#songs').append(makeSongDiv(item))
-    }
-
-    $('#songs').data('current-page', pageNumber)
-  })
-}
-
-function makeSongDiv(data) {
-  let item = $('<li></li>').addClass('song')
-  let title = '<em>' + data.title + '</em>'
-
-  return item.html(data.artist + ', ' + title + ', ' + data.year)
-}
+// Example code from previous ajax work:
+// $(document).ready(function () {
+//   console.log('ready!')
+//
+//   $('.controls button').on('click', handleClick)
+// })
+//
+// function handleClick(event) {
+//   event.preventDefault()
+//   let currentPage = $('#songs').data('current-page')
+//
+//   getPage(currentPage + 1)
+// }
+//
+// function getPage(pageNumber) {
+//   $.ajax('http://localhost:3000/songs', {
+//     type: 'GET',
+//     data: {
+//       page: pageNumber
+//     }
+//   }).done(function (data, statusCode, hxrGarbage) {
+//     console.log("Data is data", data)
+//
+//     $('#songs').html('')
+//
+//     for (let item of data) {
+//       $('#songs').append(makeSongDiv(item))
+//     }
+//
+//     $('#songs').data('current-page', pageNumber)
+//   })
+// }
+//
+// function makeSongDiv(data) {
+//   let item = $('<li></li>').addClass('song')
+//   let title = '<em>' + data.title + '</em>'
+//
+//   return item.html(data.artist + ', ' + title + ', ' + data.year)
+// }
